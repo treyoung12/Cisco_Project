aa377cc3a149ceb4077753e324b8628a66e8a833
Group-2-Cisco-Proj <38819918+Group-2-Cisco-Proj@users.noreply.github.com>
2018-04-30 17:00:24 -0400

    Add files via upload

---
+++ forgotpsw.js
@@ -0,0 +1,20 @@
+function sendPw(){
+    var auth = firebase.auth();
+    var emailAddress = document.getElementById("email").value;
+
+    auth.sendPasswordResetEmail(emailAddress).then(function() {
+
+        // Email sent.
+        alert("Sending email...");
+        location = 'index.html';
+    }).catch(function(error) {
+
+        // An error happened.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        alert("ERROR: " + errorMessage);
+        location = 'index.html'
+
+    });
+}
